d8d1ecfdd3c66f03e156663e9f66c793905bb84c
Johndhee <you@example.com>
2021-04-08 11:15:14 -0700

    Raymond/Johndhee DONE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.getElementById('create-task-form').addEventListener('submit', addTask)
+})
+
+function addTask(event) {
+  event.preventDefault()
+  let input = event.target.querySelector('input').value
+  const container = document.querySelector('#tasks')
+  const liTag = document.createElement('li')
+  liTag.textContent = input
+  container.appendChild(liTag)
+  event.target.querySelector('input').value = ""
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = "X";
+  deleteButton.onclick - deleteItem;
+  liTag.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click', function(){
+    deleteButton.remove()
+    liTag.remove()
+  })
+}
+
+function deleteItem(){
+  let itemId = this.parentNode.id;
+  document.getElementById(itemId).remove();
+}
